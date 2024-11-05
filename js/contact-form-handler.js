7abf3664ced496e7dc827d2743b3b757704b2d81
Mario Monzón <m.a.monzondelgado@gmail.com>
2024-11-05 14:52:21 +0000

    Create contact-form-handler.js

---
+++ js/contact-form-handler.js
@@ -0,0 +1,4 @@
+// Lógica para manejar envíos de formularios de contacto o solicitudes de stock
+function submitContactForm() {
+    alert("Formulario de contacto enviado. Nos pondremos en contacto contigo pronto.");
+}
